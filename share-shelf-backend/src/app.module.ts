881a9215b908014574b6587d646a76982d250402
Nirwan Rai <nirwanrai6969@gmail.com>
2025-11-08 12:24:59 +0545

    :bug: fix jwt problem

--- share-shelf-backend/src/app.module.ts
+++ share-shelf-backend/src/app.module.ts
@@ -2,9 +2,19 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
+import { PrismaModule } from './prisma.module';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [AuthModule],
+  imports: [
+    AuthModule,
+    PrismaModule,
+    JwtModule.register({
+      global: true,
+      secret: process.env.JWT_SECRET || 'supersecret',
+      signOptions: { expiresIn: '1d' },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- share-shelf-backend/src/auth/auth.module.ts
+++ share-shelf-backend/src/auth/auth.module.ts
@@ -2,14 +2,17 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { ConfigModule } from '@nestjs/config';
+import { PrismaModule } from 'src/prisma.module';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  controllers: [AuthController],
-  providers: [AuthService],
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+    PrismaModule,
   ],
+  controllers: [AuthController],
+  providers: [AuthService],
 })
 export class AuthModule {}
